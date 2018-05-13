cc6caef8437775b34993736cc39fdf6bde13de6e
David Benicek <david.benicek@skyscanner.net>
2018-05-13 16:03:13 +0100

    setting up CI

--- server.js
+++ server.js
@@ -1,22 +1,103 @@
+'use strict';
+/*-----------------------------------------------------------------------------
+A simple Language Understanding (LUIS) bot for the Microsoft Bot Framework. 
+-----------------------------------------------------------------------------*/
+
 var restify = require('restify');
 var builder = require('botbuilder');
+var botbuilder_azure = require("botbuilder-azure");
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
+  
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata 
 });
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+/*----------------------------------------------------------------------------------------
+* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+* ---------------------------------------------------------------------------------------- */
+
+// var tableName = 'botdata';
+// var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+// var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
+// Create your bot with a function to receive messages from the user
+// This default message handler is invoked if the user's utterance doesn't
+// match any intents handled by other dialogs.
+var bot = new builder.UniversalBot(connector, function (session, args) {
+    session.send('You reached the default message handler. You said \'%s\'.', session.message.text);
 });
+
+bot.set('storage', tableStorage);
+
+// Make sure you add code to validate these fields
+var luisAppId = process.env.LuisAppId;
+var luisAPIKey = process.env.LuisAPIKey;
+var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+
+const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v2.0/apps/' + luisAppId + '?subscription-key=' + luisAPIKey;
+
+// Create a recognizer that gets intents from LUIS, and add it to the bot
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+bot.recognizer(recognizer);
+
+// Add a dialog for each intent that the LUIS app recognizes.
+// See https://docs.microsoft.com/en-us/bot-framework/nodejs/bot-builder-nodejs-recognize-intent-luis 
+// CreateNote dialog
+bot.dialog('Flight.Book', [(session,reply,next) => {
+  //Try get all the data from the initial user query
+  const origin = builder.EntityRecognizer.findEntity(reply.entities, 'place::origin');
+  const destination = builder.EntityRecognizer.findEntity(reply.entities, 'place::destination');    
+  
+  //Save to dialog data
+  const trip = session.dialogData.trip = {
+      origin : origin ? origin.entity : undefined,
+      destination : destination ? destination.entity : undefined
+  }
+
+
+  //If there's no from param, ask!
+  if (!trip.origin) {
+      builder.Prompts.text(session, 'Where would you like to fly from?');
+  } else {
+      next();
+  }
+}, (session,reply,next) => {
+  if(reply.response)
+      session.dialogData.trip.origin = reply.response;
+  
+  const trip = session.dialogData.trip;
+
+
+  //If there's no from param, ask!
+  if (!trip.destination) {
+      builder.Prompts.text(session, 'Where are you flying to?');
+  } else {
+      next();
+  }
+  
+}, (session,reply) => {
+  if(reply.response)
+      session.dialogData.trip.destination =  reply.response;
+
+  const trip = session.dialogData.trip;
+
+  session.send(`Booking flight from ${ trip.origin} to ${ trip.destination}`);
+
+}]).triggerAction({ 
+  matches: 'Flight.Book',
+  confirmPrompt: "This will cancel the creation of the note you started. Are you sure?",
+});
+
