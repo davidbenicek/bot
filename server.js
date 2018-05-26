0e4004321b161a7053982de239cde82d7038ae0c
David Benicek <david.benicek@skyscanner.net>
2018-05-26 17:15:47 +0100

    Implemented secrets management

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
-'use strict';
+const restify = require('restify');
+const builder = require('botbuilder');
+require('dotenv').load();
 
-var restify = require('restify');
-var builder = require('botbuilder');
-var botbuilder_azure = require("botbuilder-azure");
+const secretsManager = require('./util/secretsManager');
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+// const botbuilderAzure = require("botbuilder-azure");
+
+if (process.env.BotEnv !== 'prod') {
+  console.log('Running in local mode... keys are already set!');
+} else {
+  console.log('Running in production...');
+  console.log('Fetching keys...');
+  const keysReady = secretsManager.loadSecrets();
+  keysReady.then((keys) => {
+    process.env = keys;
+    console.log('Keys retrieved...');
+  }).catch((err) => {
+    console.log('Failed to fetch keys...', err);
+  });
+}
+
+
+const server = restify.createServer();
+server.listen(process.env.PORT || 3978, () => {
+  console.log(`Listening on port ${process.env.PORT || 3978}`);
 });
 
-// Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    openIdMetadata: process.env.BotOpenIdMetadata 
+const connector = new builder.ChatConnector({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD,
+  openIdMetadata: process.env.BotOpenIdMetadata,
 });
 
-// Listen for messages from users 
+// Listen for messages from users
 server.post('/api/messages', connector.listen());
 
 /*----------------------------------------------------------------------------------------
@@ -27,72 +42,68 @@ server.post('/api/messages', connector.listen());
 * ---------------------------------------------------------------------------------------- */
 
 // var tableName = 'botdata';
-// var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
-// var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+// var azureTableClient = new botbuilderAzure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+// var tableStorage = new botbuilderAzure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
 // Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
+const bot = new builder.UniversalBot(connector);
 // bot.set('storage', tableStorage);
 
 // Make sure you add code to validate these fields
-var luisAppId = process.env.LuisAppId;
-var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+const { LUIS_APP_ID } = process.env;
+const { LUIS_APP_KEY } = process.env;
+const LUIS_API_HOSTNAME = process.env.LUIS_API_HOSTNAME || 'westus.api.cognitive.microsoft.com';
 
-const LuisModelUrl = '<<PUT A URL HERE>>'
+const LUIS_URL = `https://${LUIS_API_HOSTNAME}/luis/v2.0/apps/${LUIS_APP_ID}?subscription-key=${LUIS_APP_KEY}&verbose=true&timezoneOffset=0&q=`
 
-// Main dialog with LUIS
-var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-.matches('Greeting', (session) => {
-    session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
-})
-.matches('Help', [(session) => {
-    session.send('You reached Help intent, you said \'%s\'.', session.message.text);
-}])
-.matches('Flight.Book', [(session,reply,next) => {
-    //Try get all the data from the initial user query
+const recognizer = new builder.LuisRecognizer(LUIS_URL);
+const intents = new builder.IntentDialog({ recognizers: [recognizer] })
+  .matches('Greeting', (session) => {
+    session.send('Hey there! How can I help?');
+  })
+  .matches('Flight.Book', [(session, reply, next) => {
+    // Try get all the data from the initial user query
     const origin = builder.EntityRecognizer.findEntity(reply.entities, 'place::origin');
     const destination = builder.EntityRecognizer.findEntity(reply.entities, 'place::destination');    
-    
-    //Save to dialog data
-    const trip = session.dialogData.trip = {
-        origin : origin ? origin.entity : undefined,
-        destination : destination ? destination.entity : undefined
-    }
+
+    // Save to dialog data
+    const trip = {
+      origin: origin ? origin.entity : undefined,
+      destination: destination ? destination.entity : undefined,
+    };
+    session.dialogData.trip = trip;
 
 
-    //If there's no from param, ask!
+    // If there's no from param, ask!
     if (!trip.origin) {
-        builder.Prompts.text(session, 'Where would you like to fly from?');
+      builder.Prompts.text(session, 'Where would you like to fly from?');
     } else {
-        next();
+      next();
     }
-}, (session,reply,next) => {
-    if(reply.response)
-        session.dialogData.trip.origin = reply.response;
-    
-    const trip = session.dialogData.trip;
+  }, (session, reply, next) => {
+    if (reply.response) {
+      session.dialogData.trip.origin = reply.response;
+    }
 
+    const { trip } = session.dialogData;
 
-    //If there's no from param, ask!
+    // If there's no from param, ask!
     if (!trip.destination) {
-        builder.Prompts.text(session, 'Where are you flying to?');
+      builder.Prompts.text(session, 'Where are you flying to?');
     } else {
-        next();
+      next();
     }
-    
-}, (session,reply) => {
-    if(reply.response)
-        session.dialogData.trip.destination =  reply.response;
+  }, (session, reply) => {
+    if (reply.response) {
+      session.dialogData.trip.destination = reply.response;
+    }
 
-    const trip = session.dialogData.trip;
+    const { trip } = session.dialogData;
 
-    session.send(`Booking flight from ${ trip.origin} to ${ trip.destination}`);
+    session.send(`Booking flight from ${trip.origin} to ${trip.destination}`);
+  }])
+  .onDefault((session) => {
+    session.send('Sorry pal, I did not understand \'%s\'.', session.message.text);
+  });
 
-}])
-.onDefault((session) => {
-    session.send('Hey, sorry, I did not understand \'%s\'.', session.message.text);
-});
-
-bot.dialog('/', intents);    
+bot.dialog('/', intents);
---
+++ util/secretsManager.js
@@ -0,0 +1,64 @@
+const KeyVault = require('azure-keyvault');
+const { AuthenticationContext } = require('adal-node');
+
+const {
+  KEY_VAULT_CLIENT_ID,
+  KEY_VAULT_SECRET,
+  KEY_VAULT_BASE_URL,
+  KEY_VAULT_LUIS_APP_ID_KEY_ID,
+  KEY_VAULT_LUIS_APP_KEY_KEY_ID,
+  KEY_VALUE_SKYSCANNER_TOKEN_KEY_ID,
+  KEY_VALUE_MICROSOFT_ID_KEY_ID,
+  KEY_VALUE_MICROSOFT_PASSWORD_KEY_ID,
+} = process.env;
+
+const secretAuthenticator = (challenge, callback) => {
+  try {
+    const context = new AuthenticationContext(challenge.authorization);
+    return context.acquireTokenWithClientCredentials(
+      challenge.resource,
+      KEY_VAULT_CLIENT_ID,
+      KEY_VAULT_SECRET,
+      (err, tokenResponse) => {
+        if (err) throw err;
+
+        const authorizationValue = `${tokenResponse.tokenType} ${tokenResponse.accessToken}`;
+        return callback(null, authorizationValue);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+const getSecret = async (client, baseUrl, keyName, keyId) => (
+  new Promise((resolve, reject) => {
+    client.getSecret(baseUrl, keyName, keyId, (err, result) => {
+      if (err) reject(err);
+      resolve(result);
+    });
+  })
+);
+
+const loadSecrets = async () => {
+  const credentials = new KeyVault.KeyVaultCredentials(secretAuthenticator);
+  const client = new KeyVault.KeyVaultClient(credentials);
+
+  const LUIS_APP_ID = (await getSecret(client, KEY_VAULT_BASE_URL, 'LUIS-APP-ID', KEY_VAULT_LUIS_APP_ID_KEY_ID)).value;
+  const LUIS_APP_KEY = (await getSecret(client, KEY_VAULT_BASE_URL, 'LUIS-APP-KEY', KEY_VAULT_LUIS_APP_KEY_KEY_ID)).value;
+  const SKYSCANNER_TOKEN = (await getSecret(client, KEY_VAULT_BASE_URL, 'SKYSCANNER-TOKEN', KEY_VALUE_SKYSCANNER_TOKEN_KEY_ID)).value;
+  const MICROSOFT_APP_ID = (await getSecret(client, KEY_VAULT_BASE_URL, 'MICROSOFT-APP-ID', KEY_VALUE_MICROSOFT_ID_KEY_ID)).value;
+  const MICROSOFT_APP_PASSWORD = (await getSecret(client, KEY_VAULT_BASE_URL, 'MICROSOFT-APP-PASSWORD', KEY_VALUE_MICROSOFT_PASSWORD_KEY_ID)).value;
+  return {
+    LUIS_APP_ID,
+    LUIS_APP_KEY,
+    SKYSCANNER_TOKEN,
+    MICROSOFT_APP_ID,
+    MICROSOFT_APP_PASSWORD,
+  };
+};
+
+module.exports = {
+  loadSecrets,
+};
